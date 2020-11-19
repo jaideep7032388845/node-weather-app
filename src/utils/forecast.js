e80ae3ba8dffa6133284b598e70c6ff01825d39a
hello <jaideepreddy34@gmail.com>
2020-11-19 11:37:16 +0530

    added humidity propertys

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
              callback('Unable to find location', undefined)
          } 
         else {
-            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain. The current humidity is  ' + body.current.humidity )
+            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain. The current humidity is  ' + body.current.humidity + "%.")
         }
     })
 }
